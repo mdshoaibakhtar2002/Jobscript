3a6de0c0b31ca41d6716cb2ca2a8122e6791b7d1
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2023-09-28 20:54:18 +0530

    Added

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -5,9 +5,14 @@ import { useState } from 'react';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import logo from "../img/logo.png";
 import { useNavigate } from 'react-router';
+import axios from 'axios';
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
+  let dictionary = {};
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
@@ -18,6 +23,19 @@ export default function SignUp() {
       navigate('/')
     }
   }
+  const handleChange = (e) =>{
+    e.preventDefault();
+    dictionary[e.target.name] = e.target.value
+  }
+  const createUser = (e) =>{
+    e.preventDefault();
+    console.log(dictionary)
+    axios.post("http://localhost:3000/api/createuser",dictionary).then(res =>{
+      console.log("Res", res)
+    }).catch((err) =>{
+      console.log(err)
+    })
+  }
   return (
     <div className="login signup">
       <div className='padding-right'>
@@ -26,24 +44,25 @@ export default function SignUp() {
       <div className='flex-column'>
         <div className='space-between'>
           <FormControl id="cred-field">
-            <TextField label="First Name" variant="outlined" size="small" defaultValue="Elvish" />
+            <TextField name = "firstName" required label="First Name" variant="outlined" size="small" onChange={(e) => handleChange(e)} />
           </FormControl>
           <FormControl id="cred-field">
-            <TextField label="Last Name" variant="outlined" size="small" defaultValue="Khandelwal" />
+            <TextField name = "lastName" label="Last Name" variant="outlined" size="small" onChange={(e) => handleChange(e)}/>
           </FormControl>
         </div>
         <FormControl id="cred-field">
-            <TextField label="Mobile No." variant="outlined" size="small" defaultValue="+1254879894" />
+            <TextField label="Mobile No." name = "mobileNumber" variant="outlined" size="small" onChange={(e) => handleChange(e)}/>
           </FormControl>
         <FormControl id="cred-field">
-          <TextField label="Email" variant="outlined" size="small" defaultValue="imdshoaibakhtar@opp.com" />
+          <TextField label="Email" name="email" variant="outlined" size="small" onChange={(e) => handleChange(e)}/>
         </FormControl>
         <FormControl id="cred-field" sx={{ m: 1 }} variant="outlined" size="small">
           <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
           <OutlinedInput
             id="outlined-adornment-password"
             type={showPassword ? 'text' : 'password'}
-            defaultValue="786@imdshoaibakhtar"
+            name = "password"
+            onChange={(e) => handleChange(e)}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -64,7 +83,8 @@ export default function SignUp() {
           <OutlinedInput
             id="outlined-adornment-password"
             type={showPassword ? 'text' : 'password'}
-            defaultValue="786@imdshoaibakhtar"
+            name ="confirmPassword"
+            onChange={(e) => handleChange(e)}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -85,7 +105,7 @@ export default function SignUp() {
         <button onClick={(e) => pageChanger(e, "login")}>Already have an account, log in</button>
       </div>
       <div className='padding'>
-        <Button variant="contained" onClick={(e) => pageChanger(e, "createaccount")} className='margin width dark'>Sign Up</Button>
+        <Button variant="contained" onClick={(e) => createUser(e)} className='margin width dark'>Sign Up</Button>
       </div>
     </div>
   )
